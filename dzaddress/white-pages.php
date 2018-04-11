118749297fa7c1270a70601cfd59a4fdd1495afb
Kinkreux <loicopondoxva@gmail.com>
2018-04-11 23:27:00 +0300

    with data created with love

---
+++ dzaddress/white-pages.php
@@ -0,0 +1,56 @@
+<?php
+$address_json = file_get_contents(__DIR__."/adress.json");
+$address_array = json_decode($address_json, true);
+
+?>
+
+<html>
+    <head>
+        <title>
+            My Crazy White Pages
+        </title>
+    </head>
+    <body>
+        <h1>
+           Welcome to My Crazy White Pages!
+        </h1>
+        <table>
+            <thead>
+                <tr>
+                    <td>
+                        First name
+                    </td>
+                    <td>
+                        Last name
+                    </td>
+                    <td>
+                        Full address with postal code
+                    </td>
+                    <td>
+                        Mobile phone number
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($address_array as $item) : ?>
+            <tr>
+                <td>
+                    <?php echo $item['firstName']?>
+                <td>
+                    <?php echo $item['lastName']?>
+                </td>
+                <td>
+                    <?php echo $item['middleName']?>
+                </td>
+                <td>
+                    <?php echo $item['address']?>
+                </td>
+                <td>
+                    <?php echo $item['phoneNumber']?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </body>
+</html>
